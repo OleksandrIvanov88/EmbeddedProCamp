1103567d089d85ec2efd33614a6fc8a559087666
Oleksandr Ivanov <ivanov.alserg@gmail.com>
2021-07-20 03:04:48 +0300

    implemintation of tble via veribals

---
+++ lesson_03/cpu_table.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char cpu_name_1[32] = "Apple M1 8 Core 3200 MHz";
+    char cpu_name_2[32] = "Intel Core i9-11900KF @ 3.50GHz";
+    char cpu_name_3[32] = "Intel Core i9-11900F @ 2.50GHz";
+
+    int cpu_mark_1 = 3767;
+    int cpu_mark_2 = 3594;
+    int cpu_mark_3 = 3577;
+
+    float price_1 = 0.0;
+    float price_2 = 587.99;
+    float price_3 = 519.99;
+    //Heading
+    printf("-----------------------------------------------------------\n");
+    printf("| %-31s | %4s | %s |\n", "CPU", "CPU Mark", "Price (USD)");
+    printf("-----------------------------------------------------------\n");
+    //Body
+    printf("%-31s %8i %11.2f\n", cpu_name_1, cpu_mark_1, price_1);
+    printf("%-31s %8i %11.2f\n", cpu_name_2, cpu_mark_2, price_2);
+    printf("%-31s %8i %11.2f\n", cpu_name_3, cpu_mark_3, price_3);
+
+    return 0;
+}
---
+++ lesson_03/prg_lang.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/******************TIOBE programming language index*************************
+ *  TIOBE ratings are calculated by counting hits of the most popular 
+ * search engines. Twenty-five search engines are used to calculate 
+ * the TIOBE index. The TIOBE Programming Community index is an indicator 
+ * of the popularity of programming languages. The index started in the year 
+ * 2001 and updated once a month https://statisticstimes.com/tech/top-computer-languages.php 
+ */
+
+struct lang_item
+{
+    uint8_t pos_2021;
+    uint8_t pos_2020;
+    char *change;
+    char *lang;
+    float raiting;
+    float trand;
+};
+
+int main(void)
+{
+    struct lang_item data[] =
+        {
+            {1, 1, " ", "C", 13.38, -3.68},
+            {2, 3, "\u2191", "Python", 11.87, +2.75},
+            {3, 2, "\u2193", "Java", 11.74, -4.54},
+            {4, 4, " ", "C++", 7.81, +1.69}};
+
+    printf("---------------------------------------------------------------\n");
+    printf("| %5s | %5s | %5s | %-10s| %5s | %5s |\n", "May 2021", "May 2020", "Change", "Language",
+                                                       "Raiting", "Trends");
+    printf("---------------------------------------------------------------\n");
+    size_t size = sizeof(data) / sizeof(struct lang_item);
+    for (size_t i = 0; i < size; ++i)
+    {
+        printf("%*s%u %*s%u %*s%s %*s%-9s %-5.2f %% %+6.2f %%\n", 5, "", data[i].pos_2021,
+                                                                 10, "", data[i].pos_2020,
+                                                                 8, "", data[i].change,
+                                                                 6, "", data[i].lang,
+                                                                 data[i].raiting, data[i].trand);
+    }
+
+    return 0;
+}
