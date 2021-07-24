160dc21fe0d0415bc50e7066e68717774fdfb375
Oleksandr Ivanov <ivanov.alserg@gmail.com>
2021-07-24 13:10:08 +0300

    Change the name of struct and add #define for size of the array

--- lesson_04/prg_lang.c
+++ lesson_04/prg_lang.c
@@ -17,17 +17,19 @@ typedef struct
     myString lang;
     float raiting;
     float trand;
-} lang_item;
+} Lang_item;
 
-void scan_data(lang_item* item);
-void print_data(const lang_item* item);
+void scan_data(Lang_item* item);
+void print_data(const Lang_item* item);
+
+#define NLINE (sizeof(data) / sizeof(Lang_item))
 
 int main(void)
 {
     printf("Enter the amount of tabel lines = ");
     int32_t lines_number = 0;
     scanf("%i", &lines_number);
-    lang_item data[lines_number];
+    Lang_item data[lines_number];
     
     for(int i = 0; i < lines_number; ++i)
     {
@@ -39,8 +41,7 @@ int main(void)
                                                                "Raiting", "Trends");
     printf("------------------------------------------------------\n");
 
-    size_t size = sizeof(data) / sizeof(lang_item);
-    for (size_t i = 0; i < size; ++i)
+    for (size_t i = 0; i < NLINE; ++i)
     {
         print_data(&data[i]);
     }
@@ -48,14 +49,14 @@ int main(void)
     return 0;
 }
 
-void scan_data(lang_item* item)
+void scan_data(Lang_item* item)
 {
     printf("Enter: Position_2021 | Position_2020 | Language | Rainting Trends |\n");
     scanf("%u %u %s %f %f", &item->pos_2021, &item->pos_2020, &item->lang[0], 
                                                      &item->raiting, &item->trand);
         
 }
-void print_data(const lang_item* item)
+void print_data(const Lang_item* item)
 {
     printf("%*s%u %*s%u %*s%-9s %-5.2f %% %+7.2f %%\n", 5, "", item->pos_2021,
                                                         10, "", item->pos_2020,
